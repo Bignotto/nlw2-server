18433ee3df32edb77e0f2fcb564e9c87c11c65e8
Thiago Bignotto <bignotto@gmail.com>
2020-08-08 14:53:54 -0300

    create user done

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -13,35 +13,49 @@ interface ScheduleItem {
 classRouter.post("/", async (req, res) => {
   const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
 
-  const createdUsers = await db("users").insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
-  const user_id = createdUsers[0];
+  const trx = await db.transaction();
 
-  const createdClasses = await db("classes").insert({
-    subject,
-    cost,
-    user_id,
-  });
+  try {
+    const createdUsers = await trx("users").insert({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    });
+    const user_id = createdUsers[0];
 
-  const class_id = createdClasses[0];
+    const createdClasses = await trx("classes").insert({
+      subject,
+      cost,
+      user_id,
+    });
 
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinute(scheduleItem.from),
-      to: convertHourToMinute(scheduleItem.to),
-    };
-  });
+    console.log(schedule);
+    const class_id = createdClasses[0];
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinute(scheduleItem.from),
+        to: convertHourToMinute(scheduleItem.to),
+      };
+    });
+    console.log(classSchedule);
 
-  const schedules = await db("class_schedule").insert(classSchedule);
+    const schedules = await trx("class_schedule").insert(classSchedule);
 
-  console.log("added ", user_id, class_id, schedules);
-  return res.send();
+    await trx.commit();
+
+    console.log("added ", user_id, class_id, schedules);
+    return res.status(201).send();
+  } catch (error) {
+    await trx.rollback();
+    console.log(error);
+
+    return res.status(400).json({
+      error: "unexpected error while creating new class",
+    });
+  }
 });
 
 export default classRouter;
